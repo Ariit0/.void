cc508c1d8fb503c2682c2f381062a764afd4317b
Ari <luangamath.ari@gmail.com>
2022-03-30 00:21:49 +1000

    Add serverless module to monorepo

---
+++ node/serverless/service/skeleton/__tests__/demo.test.ts
@@ -0,0 +1,11 @@
+import { Demo } from "../src/lib/Demo";
+
+test("greeting", () => {
+	const demo = new Demo();
+	expect(demo.greeting("Jack Bauer")).toBe("Hello Jack Bauer");
+});
+
+test("hello_world", () => {
+	const demo = new Demo();
+	expect(demo.helloWorld()).toBe("Hello World");
+});
