fd57546202739bcfea8065d3a08faac043d6299a
Ari <luangamath.ari@gmail.com>
2022-04-18 21:23:27 +1000

    Remove @next/mdx config

--- node/blog/next.config.js
+++ node/blog/next.config.js
@@ -10,22 +10,3 @@ const nextConfig = {
 };
 
 module.exports = nextConfig;
-
-/**
- * @next/mdx configuration
- * https://nextjs.org/docs/advanced-features/using-mdx
- */
-const withMDX = require('@next/mdx')({
-	extension: /\.mdx?$/,
-	options: {
-	  remarkPlugins: [],
-	  rehypePlugins: [],
-	  // If you use `MDXProvider`, uncomment the following line.
-	  // providerImportSource: "@mdx-js/react",
-	},
-  })
-  
-  module.exports = withMDX({
-	// Append the default value with md extensions
-	pageExtensions: ['ts', 'tsx', 'js', 'jsx', 'md', 'mdx'],
-  })
