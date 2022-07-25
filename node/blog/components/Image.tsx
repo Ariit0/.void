ffb7b5dd7fe45233ab0af60eab5af122ec614536
Ari <luangamath.ari@gmail.com>
2022-07-25 23:09:41 +1000

    Finally a proper image optimizer at build time for static webpages

--- node/blog/components/Image.tsx
+++ node/blog/components/Image.tsx
@@ -1,48 +1,16 @@
-import NextImage, { ImageLoaderProps, ImageProps as NextImageProps } from "next/image";
+import ExportedImage, { ExportedImageProps } from "next-image-export-optimizer";
 import utilStyles from "../styles/utils.module.css";
 
-interface ImageProps extends NextImageProps {
+interface ImageProps extends ExportedImageProps {
 	circle?: boolean;
 }
 
-const customLoader = ({ src, width, quality }: ImageLoaderProps) => {
-	// Generate a reasonably unique base folder. Doesn't have to be perfectly unique,
-	const [path, extension] = src.split(/\.([^.]*$)/) || [];
-	// We cannot optimise SVGs via jimp
-	if (extension === "svg") throw new Error(`SVGs are not supported!`);
-
-	// Do not bother to optimize images in development
-	if (process.env.NODE_ENV === "development") {
-		return `${src}?width=${width}`;
-	}
-
-	if (!path || !extension) {
-		throw new Error(`Invalid path or no file extension: ${src}`);
-	}
-
-	const filename = path.match(/([^\/]+)$/)?.[1] || "";
-
-	const output = `/_optimized${path}/${filename}_${width}_${quality || 75}.${extension}`;
-
-	if (typeof window === "undefined") {
-		const json = { output, src, width, quality: quality || 75 };
-		const fs = require("fs");
-		const path = require("path");
-		fs.appendFileSync(
-			path.join(process.env.DIRNAME, ".next/custom-optimised-images.nd.json"),
-			JSON.stringify(json) + "\n"
-		);
-	}
-
-	return output;
-};
-
 const Image = (props: ImageProps) => {
-	let { circle, ...rest } = props;
+	let { circle, src, ...rest } = props;
 	let _className = "";
 	if (circle) _className = utilStyles.borderCircle;
 
-	return <NextImage className={`${props.className} ${_className}`} {...rest} loader={customLoader} />;
+	return <ExportedImage className={`${props.className} ${_className}`} src={src} {...rest} />;
 };
 
 export default Image;
--- node/blog/next.config.js
+++ node/blog/next.config.js
@@ -1,12 +1,20 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
 	reactStrictMode: true,
-	env: {
-		DIRNAME: __dirname
-	},
 	images: {
-		loader: "custom"
-	}
+		loader: "custom",
+		imageSizes: [64, 128, 256, 384],
+		deviceSizes: [1080, 1920, 2048, 3840],
+		nextImageExportOptimizer: {
+		  imageFolderPath: "public/images",
+		  exportFolderPath: "out",
+		  quality: 75,
+		},
+	},
+	env: {
+		storePicturesInWEBP: true,
+		generateAndUseBlurImages: true,
+	},
 };
 
 module.exports = nextConfig;
